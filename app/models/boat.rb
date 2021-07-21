cffd05d74264c1eb0397ad00893974e7486ee02a
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-07-21 13:53:34 -0600

    commit tree 27d0a8435ab3e411108e17dfb255ab8748dd9a19

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,27 +4,32 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+     limit(5)  
+     #Uses the limit method to return the first 5 boat instance records.
   end
 
   def self.dinghy
-    # where("length < 20")
+    where("length < 20")  
+    #Conditional using where to set the conditional of the length attribute to return all boat instance records that are less than 20 feet.
   end
 
   def self.ship
-    # where("length >= 20")
+     where("length >= 20")  
+     #Conditional using where to set the conditional of the length attribute to return all boat instance records that are equal or greater than 20 feet.
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+     all.order(name: :desc).limit(3) 
+     #Retreives the first three boat instance records in descending alphabetical order. order organizes the boats in alphabetical order based on name. (name: desc) has the records return in descending order. limit(3) calls for the first 3 results. 
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
-  end
+     where(captain_id: nil)  
+    #Conditional using where to set the conditional of the captain_id attribute to return the boats that are not linked to a captain.
+    end
 
   def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+     includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
@@ -32,14 +37,23 @@ class Boat < ActiveRecord::Base
     # regularly. Just know that we can get this out of the database in
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*") 
+    #Returns the boat instance records that have three classifications
+    #joins(:classification) returns the boat objects that contain classifications.
+    #group("boats.id").having('COUNT(*) = 3') Sets group up to return boat.  having('COUNT(*) 3') set the conditions of the group.
+    #select("boats.*") selects the boat attribute to return the specific boat records that meet the conditions.
   end
 
   def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
+    #Conditional using where to to return the boat records that are not in sailboats
+    #id NOT IN (?) will set up the conditional so we can return all the elements that doesn't match the criterea.
+    #self.sailboats.pluck(:id) this will replace the value in the question mark.  Here we specify by :id because there can only be one boat classification with a certain :id
+    #pluck can be used to query single or multiple columns from the underlying table of a model. It accepts a list of column names as an argument and returns an array of values of the specified columns with the corresponding data type.
   end
 
   def self.longest
-    # order('length DESC').first
+    order('length DESC').first
+    #orders all boats based on length in decending order and returns the first record.
   end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -3,11 +3,15 @@ class Classification < ActiveRecord::Base
   has_many :boats, through: :boat_classifications
 
   def self.my_all
-    # all
+     all
+     #all returns all instances of the classification class.
   end
 
   def self.longest
-    # Boat.longest.classifications
+     Boat.longest.classifications
+     #returns classifications with the longest boat
+     #Calls the boat.longest method to return the longest boat.
+     #calls classifications to return the classification instance records.
   end
 
 end
